be389bc81a49fb294dbeea6045c562d0cdc34bdc
Nuno Silva <nuno_nfss96@hotmail.com>
2022-12-10 00:58:33 +0000

    Supports multiple gamemodes

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::error::Error;
 use std::process::Command;
 
 use menu_rs::{Menu, MenuOption};
-use server::ops::{install_server, start_server};
+use server::ops::{install_server, run_server_menu};
 use utils::paths::{get_app_dir_path, get_steamcmd_exe_path};
 
 fn main() {
@@ -37,23 +37,12 @@ fn verify_os() -> Result<(), Box<dyn Error>> {
 
 fn run() -> Result<(), Box<dyn Error>> {
     let menu = Menu::new(vec![
-        MenuOption::new("Run server", _run_server).hint("Runs the server"),
+        MenuOption::new("Run server", run_server_menu).hint("Runs the server"),
         MenuOption::new("Install server", _install_server).hint("Server installation/verification"),
     ]);
     menu.show();
 
     Ok(())
-}
-
-fn _run_server() {
-    // builds path to the CSGO server folder
-    let mut server_dir_path = get_app_dir_path().expect("Failed to get the application path");
-    server_dir_path.push("server/");
-
-    match start_server(&server_dir_path) {
-        Ok(_) => {}
-        Err(_) => println!("Failed to start the server. Make sure the server is installed before trying to run it."), 
-    }
 }
 
 fn _install_server() {
--- src/server/ops.rs
+++ src/server/ops.rs
@@ -2,7 +2,9 @@ use std::error::Error;
 use std::path::Path;
 use std::process::Command;
 
-use crate::utils::paths::get_steamcmd_exe_path;
+use menu_rs::{Menu, MenuOption};
+
+use crate::utils::paths::{get_app_dir_path, get_steamcmd_exe_path};
 
 pub fn install_server(dir_path: &Path) -> Result<(), Box<dyn Error>> {
     let dir_path_str = dir_path.to_str().unwrap();
@@ -24,18 +26,66 @@ pub fn install_server(dir_path: &Path) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-pub fn start_server(dir_path: &Path) -> Result<(), Box<dyn Error>> {
+pub fn run_server_menu() {
+    let menu = Menu::new(vec![
+        MenuOption::new("Classic Competitive", start_classic_competitive_server),
+        MenuOption::new("Classic Casual", start_classic_casual_server),
+        MenuOption::new("Arms Race", start_arms_race_server),
+        MenuOption::new("Demolition", start_demolition_server),
+        MenuOption::new("Deathmatch", start_deathmatch_server),
+    ]);
+    menu.show();
+}
+
+fn start_classic_competitive_server() {
+    let args =
+        "-game csgo -console -usercon +game_type 0 +game_mode 1 +mapgroup mg_active +map de_dust2";
+    start_server(args);
+}
+
+fn start_classic_casual_server() {
     let args =
         "-game csgo -console -usercon +game_type 0 +game_mode 0 +mapgroup mg_active +map de_dust2";
+    start_server(args);
+}
+
+fn start_arms_race_server() {
+    let args = "-game csgo -console -usercon +game_type 1 +game_mode 0 +mapgroup mg_armsrace +map ar_shoots";
+    start_server(args);
+}
+
+fn start_demolition_server() {
+    let args = "-game csgo -console -usercon +game_type 1 +game_mode 1 +mapgroup mg_demolition +map de_lake";
+    start_server(args);
+}
+
+fn start_deathmatch_server() {
+    let args = "-game csgo -console -usercon +game_type 1 +game_mode 2 +mapgroup mg_allclassic +map de_dust";
+    start_server(args);
+}
+
+fn start_server(game_mode_args: &str) {
+    match _start_server(game_mode_args) {
+        Ok(_) => {}
+        Err(_) => println!("Failed to start the server. Make sure the server is installed before trying to run it."), 
+    }
+}
+
+fn _start_server(game_mode_args: &str) -> Result<(), Box<dyn Error>> {
+    // builds path to the CSGO server folder
+    let mut server_dir_path = get_app_dir_path().expect("Failed to get the application path");
+    server_dir_path.push("server/");
+
     if cfg!(windows) {
+        let windows_args = format!("/c srcds {}", game_mode_args);
         Command::new("cmd")
-            .current_dir(dir_path)
-            .arg("/c srcds -game csgo -console -usercon +game_type 0 +game_mode 0 +mapgroup mg_active +map de_dust2")
+            .current_dir(server_dir_path)
+            .arg(windows_args)
             .status()?;
     } else if cfg!(unix) {
         Command::new("./srcds_run")
-            .current_dir(dir_path)
-            .arg(args)
+            .current_dir(server_dir_path)
+            .arg(game_mode_args)
             .status()?;
     } else {
         Err("OS not supported")?;
