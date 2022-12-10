9d96d570d8a4a9f326469347aa10abb74423b05c
Nuno Silva <nuno_nfss96@hotmail.com>
2022-12-10 19:42:17 +0000

    Handle exec.txt with `load_exec()`

--- src/utils/config.rs
+++ src/utils/config.rs
@@ -5,7 +5,68 @@ use std::path::Path;
 
 use crate::utils::paths::get_app_dir_path;
 
-pub fn load_cfg(filename: &str) -> Result<Vec<String>, Box<dyn Error>> {
+pub fn load_commands() -> Result<Vec<String>, Box<dyn Error>> {
+    let mut file_path = get_app_dir_path()?;
+    file_path.push("config/commands.txt");
+
+    let reader = match load_file(&file_path) {
+        Ok(r) => r,
+        Err(_) => {
+            println!("Can't load \"config/commands.txt\"");
+            Err("Error loading commands")?
+        }
+    };
+
+    let mut entries = Vec::new();
+    for line in reader.lines() {
+        let mut line = line?;
+        line = line.trim().to_owned();
+
+        let ignore_line = line.starts_with('/') || line.is_empty();
+        if ignore_line {
+            continue;
+        }
+
+        // removes extra white spaces between words
+        let words: Vec<_> = line.split_whitespace().collect();
+        line = words.join(" ");
+
+        // removes comments
+        let words: Vec<_> = line.split(" //").collect();
+        line = words[0].to_owned();
+
+        // builds commands
+        let command_str = format!("-{line}");
+
+        entries.push(command_str);
+    }
+
+    Ok(entries)
+}
+
+pub fn load_exec() -> Result<Vec<String>, Box<dyn Error>> {
+    let mut file_path = get_app_dir_path()?;
+    file_path.push("config/exec.txt");
+
+    let exec_reader = match load_file(&file_path) {
+        Ok(r) => r,
+        Err(_) => {
+            println!("Can't load \"config/exec.txt\"");
+            Err("Error loading config/exec.txt")?
+        }
+    };
+
+    let mut entries = Vec::new();
+    for line in exec_reader.lines() {
+        let line = line?;
+        let mut values = load_cfg(&line)?;
+        entries.append(&mut values);
+    }
+
+    Ok(entries)
+}
+
+fn load_cfg(filename: &str) -> Result<Vec<String>, Box<dyn Error>> {
     let mut file_path = get_app_dir_path()?;
     file_path.push("cfgs/");
     file_path.push(filename);
@@ -40,43 +101,6 @@ pub fn load_cfg(filename: &str) -> Result<Vec<String>, Box<dyn Error>> {
         let command_str = format!("+{line}");
 
         entries.push(command_str);
-    }
-
-    Ok(entries)
-}
-
-pub fn load_commands() -> Result<Vec<String>, Box<dyn Error>> {
-    let mut file_path = get_app_dir_path()?;
-    file_path.push("config/commands.txt");
-
-    let reader = match load_file(&file_path) {
-        Ok(r) => r,
-        Err(_) => {
-            println!("Can't load \"config/commands.txt\"");
-            Err("Error loading commands")?
-        }
-    };
-
-    let mut entries = Vec::new();
-    for line in reader.lines() {
-        let mut line = line?;
-        line = line.trim().to_owned();
-
-        let ignore_line = line.starts_with('/') || line.is_empty();
-        if ignore_line {
-            continue;
-        }
-
-        // removes extra white spaces between words
-        let words: Vec<_> = line.split_whitespace().collect();
-        line = words.join(" ");
-
-        // removes comments
-        let words: Vec<_> = line.split(" //").collect();
-        line = words[0].to_owned();
-
-        // builds commands
-        let command_str = format!("-{line}");
     }
 
     Ok(entries)
