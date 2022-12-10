c775b5f2b64dfc9dbc34474e881432919eaf62d8
Nuno Silva <nuno_nfss96@hotmail.com>
2022-12-10 19:10:59 +0000

    Load command file with `load_commands)`

--- src/utils/config.rs
+++ src/utils/config.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
+use std::path::Path;
 
 use crate::utils::paths::get_app_dir_path;
 
@@ -9,14 +10,15 @@ pub fn load_cfg(filename: &str) -> Result<Vec<String>, Box<dyn Error>> {
     file_path.push("cfgs/");
     file_path.push(filename);
 
-    if !file_path.exists() {
-        println!("Can't load \"{}\"", filename);
-        Err("Error loading .cfg")?;
-    }
+    let reader = match load_file(&file_path) {
+        Ok(r) => r,
+        Err(_) => {
+            println!("Can't load \"{}\"", filename);
+            Err("Error loading .cfg")?
+        }
+    };
 
     let mut entries = Vec::new();
-    let file = File::open(file_path)?;
-    let reader = BufReader::new(file);
     for line in reader.lines() {
         let mut line = line?;
         line = line.trim().to_owned();
@@ -36,10 +38,57 @@ pub fn load_cfg(filename: &str) -> Result<Vec<String>, Box<dyn Error>> {
 
         // builds commands
         let command_str = format!("+{line}");
-        println!("aaaaa {}", command_str);
 
         entries.push(command_str);
     }
 
     Ok(entries)
+}
+
+pub fn load_commands() -> Result<Vec<String>, Box<dyn Error>> {
+    let mut file_path = get_app_dir_path()?;
+    file_path.push("config/commands.txt");
+
+    let reader = match load_file(&file_path) {
+        Ok(r) => r,
+        Err(_) => {
+            println!("Can't load \"config/commands.txt\"");
+            Err("Error loading commands")?
+        }
+    };
+
+    let mut entries = Vec::new();
+    for line in reader.lines() {
+        let mut line = line?;
+        line = line.trim().to_owned();
+
+        let ignore_line = line.starts_with('/') || line.is_empty();
+        if ignore_line {
+            continue;
+        }
+
+        // removes extra white spaces between words
+        let words: Vec<_> = line.split_whitespace().collect();
+        line = words.join(" ");
+
+        // removes comments
+        let words: Vec<_> = line.split(" //").collect();
+        line = words[0].to_owned();
+
+        // builds commands
+        let command_str = format!("-{line}");
+    }
+
+    Ok(entries)
+}
+
+fn load_file(file_path: &Path) -> Result<BufReader<File>, Box<(dyn std::error::Error + 'static)>> {
+    if !file_path.exists() {
+        Err("Error loading file")?;
+    }
+
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+
+    Ok(reader)
 }
