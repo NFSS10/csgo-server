b75b31ee8ea816a3c88be2f405b4dda53ba77e7d
Nuno Silva <22588915+NFSS10@users.noreply.github.com>
2023-04-08 23:41:40 +0100

    Prettify

--- csgo-server/src/App.vue
+++ csgo-server/src/App.vue
@@ -1,33 +1,33 @@
 <template>
-  <router-view v-slot="{ Component }">
-      <transition name="fade" mode="out-in">
-          <component class="page" :is="Component" />
-      </transition>
-  </router-view>
+    <router-view v-slot="{ Component }">
+        <transition name="fade" mode="out-in">
+            <component class="page" :is="Component" />
+        </transition>
+    </router-view>
 </template>
 
 <style scoped>
 #app {
-  display: flex;
-  flex-direction: column;
-  box-sizing: border-box;
-  height: 100%;
-  min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    box-sizing: border-box;
+    height: 100%;
+    min-height: 100vh;
 }
 
 #app > .fade-enter-active,
 #app > .fade-leave-active {
-  transition: opacity 0.15s ease-in-out;
+    transition: opacity 0.15s ease-in-out;
 }
 
 #app > .fade-enter-from,
 #app > .fade-leave-to {
-  opacity: 0;
+    opacity: 0;
 }
 </style>
 
 <script lang="ts">
 export default {
-  name: "App"
+    name: "App"
 };
 </script>
